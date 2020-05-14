90c72a5a7ee57e63cda60a2fd5722b4aa417c3ae
Aravind Balla <bsaaravind@gmail.com>
2020-05-14 15:47:59 +0530

    fix: add the animation styles properly

--- src/gatsby-theme-andy/components/MdxComponents.js
+++ src/gatsby-theme-andy/components/MdxComponents.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Tippy from '@tippyjs/react';
-import 'tippy.js/animations/shift-away.css';
+
+// Animation styles are imported in `src/styles.css`
 
 const AnchorTag = ({ href, popups = {}, ...restProps }) => {
   if (!href.match(/^http/))
